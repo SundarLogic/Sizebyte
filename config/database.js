d7bb36d23bec297fe6ef769f6209fca8940ef40a
SundarLogic <sundaramahalingamt98@gmail.com>
2026-09-07 23:56:49 +0530

    Fix mysql2 bundling for Vercel

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,6 +9,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+    dialectModule: mysql2,
 
     ...(process.env.NODE_ENV === "production" && {
       dialectOptions: {
